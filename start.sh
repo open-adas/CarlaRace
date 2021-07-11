7b92a454d8ff8e79e2fd87bb9858cfaee41d2b07
sid1057 <ivanov.dale@gmail.com>
2021-07-11 13:18:16 +0300

    bugfix

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ echo loading...
 sleep 10
 cd $CARLA_ROOT/PythonAPI/util
 python3 config.py -m Town04 --fps 10 --weather CloudyNoon
-cd .$CARLA_ROOT/PythonAPI/examples
+cd $CARLA_ROOT/PythonAPI/examples
 python3 spawn_npc.py --car-lights-on --sync --safe -w 0 -n 25
 
 echo ready... steady... go!
